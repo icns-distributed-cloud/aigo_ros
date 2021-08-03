e7056d42464314fe16ab897f175a3ff504a649bf
easyblank <clapd10@khu.ac.kr>
2021-08-03 14:50:56 +0900

    made files executable on AIGO:

--- aigo_serial/src/aigo_serial_sub.py
+++ aigo_serial/src/aigo_serial_sub.py
@@ -8,15 +8,22 @@ ser_front = serial.Serial(\
     port = '/dev/ttyUSB0', \
     baudrate = 115200,
 )
-def callback(msg):
-    print(msg.data)
+
+lwheel_desired_rate = 0
+rwheel_desired_rate = 0
+
+def left_callback(msg):
+    lwheel_desired_rate = msg.data
+
+def right_callback(msg):
+    rwheel_desired_rate = msg.data
 
 if __name__ == '__main__':
     rospy.init_node('aigo_serial_sub')
-    lwheel_desired_rate_sub = rospy.Subscriber('lwheel_desired_rate', Int32, callback)
-    rwheel_desired_rate_sub = rospy.Subscriber('rwheel_desired_rate', Int32, callback)
-    lwheel_desired_rate = lwheel_desired_rate_sub.data
-    rwheel_desired_rate = rwheel_desired_rate_sub.data
+    lwheel_desired_rate_sub = rospy.Subscriber('lwheel_desired_rate', Int32, left_callback)
+    time.sleep(0.1)
+    rwheel_desired_rate_sub = rospy.Subscriber('rwheel_desired_rate', Int32, right_callback)
+    
     while not rospy.is_shutdown():
         time.sleep(0.1)
         ser_front.write(lwheel_desired_rate, b',', rwheel_desired_rate)
